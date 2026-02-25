444efe9f916d2e7ed965a11d9aea634349d92205
vlad <vlad@vg.codes>
2026-02-25 23:14:58 +0400

    Fix onboarding action and analytics events

--- adapty/src/commonMain/kotlin/com/adapty/kmp/internal/plugin/response/AdaptyOnboardingsAnalyticsEventResponse.kt
+++ adapty/src/commonMain/kotlin/com/adapty/kmp/internal/plugin/response/AdaptyOnboardingsAnalyticsEventResponse.kt
@@ -5,6 +5,8 @@ import com.adapty.kmp.models.AdaptyOnboardingsAnalyticsEventOnboardingCompleted
 import com.adapty.kmp.models.AdaptyOnboardingsAnalyticsEventOnboardingStarted
 import com.adapty.kmp.models.AdaptyOnboardingsAnalyticsEventProductsScreenPresented
 import com.adapty.kmp.models.AdaptyOnboardingsAnalyticsEventRegistrationScreenPresented
+import com.adapty.kmp.models.AdaptyOnboardingsAnalyticsEventScreenCompleted
+import com.adapty.kmp.models.AdaptyOnboardingsAnalyticsEventScreenPresented
 import com.adapty.kmp.models.AdaptyOnboardingsAnalyticsEventSecondScreenPresented
 import com.adapty.kmp.models.AdaptyOnboardingsAnalyticsEventUnknown
 import com.adapty.kmp.models.AdaptyOnboardingsAnalyticsEventUserEmailCollected
@@ -16,14 +18,18 @@ import kotlinx.serialization.Serializable
 internal data class AdaptyOnboardingsAnalyticsEventResponse(
     @SerialName("name")
     val name: String,
+    @SerialName("element_id")
+    val elementId: String? = null,
+    @SerialName("reply")
+    val reply: String? = null,
 )
 
 internal fun AdaptyOnboardingsAnalyticsEventResponse.asAdaptyOnboardingEvent(): AdaptyOnboardingsAnalyticsEvent {
 
     return when (this.name) {
         "onboarding_started" -> AdaptyOnboardingsAnalyticsEventOnboardingStarted
-        "screen_presented" -> AdaptyOnboardingsAnalyticsEventOnboardingStarted
-        "screen_completed" -> AdaptyOnboardingsAnalyticsEventOnboardingStarted
+        "screen_presented" -> AdaptyOnboardingsAnalyticsEventScreenPresented
+        "screen_completed" -> AdaptyOnboardingsAnalyticsEventScreenCompleted(elementId = this.elementId, reply = this.reply)
         "second_screen_presented" -> AdaptyOnboardingsAnalyticsEventSecondScreenPresented
         "registration_screen_presented" -> AdaptyOnboardingsAnalyticsEventRegistrationScreenPresented
         "products_screen_presented" -> AdaptyOnboardingsAnalyticsEventProductsScreenPresented
